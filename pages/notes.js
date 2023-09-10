21a26ba77d1e28d19ef5821241c12c3f6fec4c2b
Jin Heung An <wlsgmd1535@naver.com>
2023-09-10 23:47:53 +0900

    Update notes.js

--- pages/notes.js
+++ pages/notes.js
@@ -1,6 +1,5 @@
 import Container from '@/components/Container'
 import NotePost from '@/components/NotePost'
-import NotesHero from '@/components/Hero/Notes'
 import { getBlocksMaps } from '@/lib/getBlocksMaps'
 import { getPostBlocks, getAllPosts } from '@/lib/notion'
 
@@ -43,10 +42,6 @@ const Notes = ({ blocksJson, siteConfigObj, blockMap }) => {
       description={siteConfigObj['Site Description']}
       siteConfigObj={siteConfigObj}
     >
-      <NotesHero blockMap={blockMap} />
-      {blocksJson.map((block) => (
-        <NotePost key={block.slug} note={block} />
-      ))}
     </Container>
   )
 }
