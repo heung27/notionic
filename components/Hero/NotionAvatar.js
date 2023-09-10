87528a235c34ba0fafbb7f7fecdb9d36dc561058
Jin Heung An <wlsgmd1535@naver.com>
2023-09-10 23:23:05 +0900

    Update NotionAvatar.js

--- components/Hero/NotionAvatar.js
+++ components/Hero/NotionAvatar.js
@@ -4,9 +4,7 @@ import Image from 'next/image'
 
 const NotionAvatar = (props) => (
   <Image
-    fill
     src='https://github.com/heung27/notionic/blob/main/public/mimo.png'
-    className='w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200'
   />
 )
 
