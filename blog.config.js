ae0d49b64e4deac1d3d5797542f85ce7fbb17b63
Jin Heung An <wlsgmd1535@naver.com>
2023-09-12 22:57:38 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'businessheung@gmail.com',
   link: 'https://github.com/heung27',
   newsletter: 'Notionic Weekly',
-  description: 'A static blog build on top of Notion and Next.js',
+  description: '',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Seoul', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
