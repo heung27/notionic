3c590138c117a31fe684f3d14028c67823840f49
Jin Heung An <wlsgmd1535@naver.com>
2023-09-16 14:25:54 +0900

    Update Home.js

--- components/Hero/Home.js
+++ components/Hero/Home.js
@@ -28,7 +28,7 @@ const Hero = ({ blockMap }) => {
   return (
     <>
       <div className='container mx-auto flex px-5 py-2 mb-10 md:flex-row flex-col items-center'>
-        <div className='flex flex-col md:w-3/5 md:items-start mb-6 md:mb-0 text-left'>
+        <div className='flex flex-col md:w-4/5 md:items-start mb-6 md:mb-0 text-left'>
           <NotionRenderer
             className='md:ml-0'
             blockMap={blockMap}
